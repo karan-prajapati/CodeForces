6febd21d2a895cc9f6c1717271d5e05eaf5fd581
karan-prajapati <karan.work.2004@gmail.com>
2022-12-16 23:11:40 +0530

    400 - 20 = 380 left

---
+++ 400 Easy Problems/16-12-22/432A Choosing Teams.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n, k, x, result {};
+	cin >> n >> k;
+	for (; n > 0; n--){
+		cin >> x;
+		if ((x + k) <= 5){
+			result++;
+		}
+	}
+	cout << result / 3 << endl;
+	return 0;
+}
