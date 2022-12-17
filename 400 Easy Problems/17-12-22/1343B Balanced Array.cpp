f350dacc2efe811699c44a62da1e4fca115a930e
karan-prajapati <karan.work.2004@gmail.com>
2022-12-17 19:11:59 +0530

    17/12/22

---
+++ 400 Easy Problems/17-12-22/1343B Balanced Array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int	main(){
+	int n, x;
+	cin >> n;
+	for (; n > 0; n--){
+		cin >> x;
+		int odd {1}, even {2}, arr[x], i {}, odd_sum {}, even_sum {};
+		if ((x / 2) % 2 == 0){
+			while (odd != even or i < x){
+				if (odd > even){
+				}
+			}
+		} else {
+			cout << "NO" << endl;
+		}
+	}
+	return 0;
+}
