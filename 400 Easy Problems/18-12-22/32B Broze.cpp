7ae6744b88abe2e5038cde69fa1e415a47ff27be
karan-prajapati <karan.work.2004@gmail.com>
2022-12-18 23:32:59 +0530

    380 - 10 = 370 left

---
+++ 400 Easy Problems/18-12-22/32B Broze.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string s;
+	cin >> s;
+	int x {};
+	while (x < s.size()){
+		if (s.at(x) == '.'){
+			cout << "0";
+		} else if (s.at(x) == '-' and s.at(x + 1) == '.'){
+			cout << "1";
+			x++;
+		} else if (s.at(x) == '-' and s.at(x + 1) == '-'){
+			cout << "2";
+			x++;
+		}
+		x++;
+	}
+	cout << endl;
+	return 0;
+}
