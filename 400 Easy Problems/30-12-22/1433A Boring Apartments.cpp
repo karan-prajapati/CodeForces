d5239b58b24070aec8ce6164f702f8b70731e046
karan-prajapati <karan.work.2004@gmail.com>
2022-12-30 21:28:18 +0530

    30/12/22

---
+++ 400 Easy Problems/30-12-22/1433A Boring Apartments.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int test_case, num;
+	cin >> test_case;
+	for (; test_case > 0; test_case--){
+		cin >> num;
+		int result {(num % 10 - 1) * 10 + 1}, count {2};
+		for (int i = 0; i < 3; i++){
+			if (num / 10 != 0){
+				result += count;
+				num /= 10;
+			}
+			count++;
+		}
+		cout << result << endl;
+	}
+	return 0;
+}
